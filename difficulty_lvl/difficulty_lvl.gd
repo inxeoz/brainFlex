fc440f29fb27106c0c686a2a5097075714743cf9
inxeoz <inxeoz@users.noreply.github.com>
2023-11-10 12:19:27 +0530

    due color rect i removed simple icon for rat, cat , dog and implemented my own , difficulty level get revisied
    Signed-off-by: inxeoz <inxeoz@users.noreply.github.com>

--- difficulty_lvl/difficulty_lvl.gd
+++ difficulty_lvl/difficulty_lvl.gd
@@ -3,7 +3,9 @@ var settings = load("res://settings/settings.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Button_easy/ColorRect.color = DarkTheme.color5
+	$Button_medium/ColorRect.color  = DarkTheme.color5
+	$Button_hard/ColorRect.color  = DarkTheme.color5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +15,39 @@ func _process(delta):
 
 func _on_button_back_pressed():
 	get_tree().change_scene_to_packed(settings);
+
+
+func _on_button_easy_button_down():
+	#$Button_easy/ColorRect.color = DarkTheme.color4
+	pass
+	
+
+
+func _on_button_easy_button_up():
+	$Button_easy/ColorRect.color = DarkTheme.lvl1
+	$Button_medium/ColorRect.color = DarkTheme.color5
+	$Button_hard/ColorRect.color = DarkTheme.color5
+	
+	
+
+
+func _on_button_medium_button_down():
+	pass
+	#$Button_medium/ColorRect.color = DarkTheme.color4
+
+
+func _on_button_medium_button_up():
+	$Button_easy/ColorRect.color = DarkTheme.color5
+	$Button_medium/ColorRect.color = DarkTheme.lvl2
+	$Button_hard/ColorRect.color = DarkTheme.color5
+
+
+func _on_button_hard_button_down():
+	pass
+	#$Button_hard/ColorRect.color = DarkTheme.color4
+
+
+func _on_button_hard_button_up():
+	$Button_easy/ColorRect.color = DarkTheme.color5
+	$Button_medium/ColorRect.color = DarkTheme.color5
+	$Button_hard/ColorRect.color = DarkTheme.lvl3
